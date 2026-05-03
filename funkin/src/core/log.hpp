6ccfa02d50d10645f55a70a6cda0856737dfa49b
Dusty <100844923+DustyDevz@users.noreply.github.com>
2026-05-03 01:37:20 -0400

    i have no fucking clue why vsc is being annoying

--- funkin/src/core/log.hpp
+++ funkin/src/core/log.hpp
@@ -76,7 +76,7 @@ namespace Funkin::Core {
 
 }
 
-#ifdef FUNKIN_DEBUG
+#ifdef FUNKIN_DEBUG 
     #define LOG_INFO(fmt, ...)  ::Funkin::Core::LogMessage(::Funkin::Core::LogLevel::Info,     __FILE__, __LINE__, fmt, ##__VA_ARGS__)
     #define LOG_WARN(fmt, ...)  ::Funkin::Core::LogMessage(::Funkin::Core::LogLevel::Warning,  __FILE__, __LINE__, fmt, ##__VA_ARGS__)
     #define LOG_ERR(fmt, ...)   ::Funkin::Core::LogMessage(::Funkin::Core::LogLevel::Error,    __FILE__, __LINE__, fmt, ##__VA_ARGS__)
