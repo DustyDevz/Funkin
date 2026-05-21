fb01afda0091e2a9164082bcb4b61a676806741c
Dusty <100844923+DustyDevz@users.noreply.github.com>
2026-05-20 20:56:24 -0400

    github failing fix

--- funkin/src/core/main.cpp
+++ funkin/src/core/main.cpp
@@ -1,11 +1,16 @@
 // © 2026 Dusty | https://github.com/DustyDevz/FNFCPP
 // Licensed under GNU GPL v3.0
 
+#include <windows.h>
+#include <stdio.h>
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
     AllocConsole();
+
     FILE* fDummy;
+
     freopen_s(&fDummy, "CONOUT$", "w", stdout);
     freopen_s(&fDummy, "CONOUT$", "w", stderr);
     freopen_s(&fDummy, "CONIN$", "r", stdin);
-    return run();
+    return 0;
 }
